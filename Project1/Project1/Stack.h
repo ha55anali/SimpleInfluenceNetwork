2718b3cb0a929292508845dec28cacebac03c2ec
Hassan Ali <Hassan Ali@DESKTOP-3JVVKJJ>
2019-09-27 23:17:59 +0500

    Implemented stack class
    used singly linked list to implement the stack

---
+++ Project1/Project1/Stack.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "list.h"
+
+template<class T>
+class Stack{
+public:
+	Stack();
+	~Stack();
+
+	void pop();
+	void push(T const&);
+
+	T& TopElement();
+
+	bool isFull();
+	bool isEmpty();
+private:
+	sList<T> s;
+};
+
+template<class T>
+Stack<T>::Stack(){
+
+}
+
+template<class T>
+Stack<T>::~Stack(){
+
+}
+
+template<class T>
+void Stack<T>::pop(){
+	s.remove(s.begin());
+}
+
+template<class T>
+void Stack<T>::push(T const& val){
+	s.insertAtStart(val);
+}
+
+template<class T>
+T& Stack<T>::TopElement()
+{
+	if (!isEmpty()){
+		return *s.begin();
+	}
+}
+
+template<class T>
+bool Stack<T>::isFull()
+{
+	return false;
+}
+
+template<class T>
+bool Stack<T>::isEmpty()
+{
+	if (s.getSize()==0) return 1;
+	return 0;
+}
